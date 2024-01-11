240be83e6daffb983bf2d585689f337ccbb00fb5
kavindu-udara <karunasinghesampath38@gmail.com>
2024-01-11 04:32:36 -0800

    created student view

--- admin/controllers/StudentController.php
+++ admin/controllers/StudentController.php
@@ -20,4 +20,14 @@ class StudentController{
             return false;
         }
     }
+
+    public function index(){
+        $studentQuery = "SELECT * FROM students";
+        $result = $this->conn->query($studentQuery);
+        if($result->num_rows > 0){
+            return $result;
+        }else{
+            return false;
+        }
+    }
 }
---
+++ admin/student-view.php
@@ -0,0 +1,73 @@
+<?php
+
+include('../config/app.php');
+include_once('../controllers/AuthenticationController.php');
+$authenticated = new AuthenticationController;
+
+$authenticated->admin();
+
+include_once('controllers/StudentController.php');
+
+include("includes/header.php");
+
+?>
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Student Add</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <?php include('../message.php'); ?>
+            <div class="card">
+                <div class="card-header">
+                    <h4>Student View</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-borders">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Full Name</th>
+                                    <th>Course</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $students = new StudentController;
+                                $result = $students->index();
+
+                                if ($result) {
+                                    foreach ($result as $row) {
+                                        ?>
+                                        <tr>
+                                            <td><?= $row['id'] ?></td>
+                                            <td><?= $row['fullname'] ?></td>
+                                            <td><?= $row['email'] ?></td>
+                                            <td>
+                                                <a href="#" class="btn btn-success">Edit</a>
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-danger">Delete</a>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                } else {
+                                    echo "No record found";
+                                }
+                                ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
